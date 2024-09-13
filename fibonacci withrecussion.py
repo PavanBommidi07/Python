0b024d92ebf82cbebc788f0c0b3a8fc0edf91694
Bommidi_Pavan <159125390+PavanBommidi07@users.noreply.github.com>
2024-09-13 22:22:12 +0530

    fibonacci_sequence .py

--- fibonacci withrecussion.py
+++ fibonacci withrecussion.py
@@ -2,12 +2,12 @@ def fib(n):
     fib_seq=[0,1]
 
     if n<=1:
-        return fib[:n+1]
+        return fib_seq[:n+1]
     else:
         for i in range(2,n+1):
-            next=fib(-1)+fib(-2)
-            fib(next)
-        return fib
+            next=fib_seq[-1] + fib_seq[-2]
+            fib_seq.append(next)
+        return fib_seq
 n=10
 print("fib sequence")
 print(fib(n))
